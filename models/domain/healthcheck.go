35acdeaae850afc534057d5fb4f07d382e736912
Paulo Coutinho <paulo@prsolucoes.com>
2016-05-20 22:02:17 -0300

    notifications impl

--- models/domain/healthcheck.go
+++ models/domain/healthcheck.go
@@ -65,6 +65,7 @@ func (This *Healthcheck) NotifyWarningStatus() {
 		for _, notifier := range This.WarningNotifiers {
 			if notifier.CanSendNotification() {
 				log.Println("Healthcheck : Started process to send warning notifications")
+				// TODO: SEND ONLY TO THIS NOTIFIER CONFIG
 				go NotifierManagerProcess(This)
 			}
 		}
@@ -76,6 +77,7 @@ func (This *Healthcheck) NotifyErrorStatus() {
 		for _, notifier := range This.ErrorNotifiers {
 			if notifier.CanSendNotification() {
 				log.Println("Healthcheck : Started process to send error notifications")
+				// TODO: SEND ONLY TO THIS NOTIFIER CONFIG
 				go NotifierManagerProcess(This)
 			}
 		}
